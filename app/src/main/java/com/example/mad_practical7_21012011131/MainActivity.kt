f7c70c8b8947e9de6860a5e0428dd2107b3be745
umang <umangraval0365@gmail.com>
2023-09-14 19:18:23 +0530

    final

--- app/src/main/java/com/example/mad_practical7_21012011131/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical7_21012011131/MainActivity.kt
@@ -3,24 +3,21 @@ package com.example.mad_practical7_21012011131
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.widget.MediaController
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val vid: VideoView = findViewById(R.id.video)
-        val vidbtn: FloatingActionButton = findViewById(R.id.videobtn)
-        vidbtn.setOnClickListener {
-            videobtn();
-            videow()
-        }
-    }
-    fun videow()
-    {
         val videoView: VideoView = findViewById(R.id.video)
+        //creating media controller
         val mediaController = MediaController(this)
         val uri:Uri = Uri.parse("android.resource://"+ packageName + "/"+ R.raw.thestoryoflight)
         mediaController.setAnchorView(videoView)
@@ -28,11 +25,15 @@ class MainActivity : AppCompatActivity() {
         videoView.setVideoURI(uri);
         videoView.requestFocus();
         videoView.start();
-
+        val vid: VideoView = findViewById(R.id.video)
+        val vidbtn: FloatingActionButton = findViewById(R.id.videobtn)
+        vidbtn.setOnClickListener{ videobtn() }
     }
+
     fun videobtn()
     {
         val intent= Intent(this,YoutubeActivity::class.java)
         startActivity(intent)
     }
 }
+
--- app/src/main/java/com/example/mad_practical7_21012011131/YoutubeActivity.kt
+++ app/src/main/java/com/example/mad_practical7_21012011131/YoutubeActivity.kt
@@ -5,19 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
-        val wvid: WebView= findViewById(R.id.web1)
         val wvidbtn: FloatingActionButton = findViewById(R.id.webbtn)
             wvidbtn.setOnClickListener {
             wvideobtn()
-            videoW()
         }
-    }
-    fun videoW()
-    {
         val yotubeid = "fJn9B64Znrk"
         val myWebView = findViewById<WebView>(R.id.web1)
         val webSettings = myWebView.settings
